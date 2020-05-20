1af4472488cb071c3585843a21f5325e84b56b4f
teamfam <teamfam2014@gmail.com>
2020-05-19 23:55:18 -0400

    variables

--- src/main/kotlin/variables.kt
+++ src/main/kotlin/variables.kt
@@ -1,6 +1,18 @@
 fun main(args: Array<String>) {
     var testVar = "testMe"
-    println(testVar)
+    println("testVar=" + testVar)
     val testVal = "testMe2"
-    println(testVal)
+    println("testVal=" + testVal)
+    //testing out primitives
+    //boolean
+    val testBoolean = 3 < 4
+    println("testBoolean=" + testBoolean)
+    //byte
+    val bigByte : Byte = 127
+    println("bigByte=" + bigByte)
+    //Float
+    val myFloat : Float = 3.542422F
+    println("myFloat=" + myFloat)
+    var myDouble : Double = myFloat.toDouble()
+    println("myDouble=" + myDouble)
 }
